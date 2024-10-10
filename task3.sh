9e770fddf345dfad62d07e6d33b93a1084045b37
lakhanij <164538484+lakhanij@users.noreply.github.com>
2024-10-10 06:07:30 -0400

    Update task3.sh

--- task3.sh
+++ task3.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-awk -F, '$3 == 2 && $NF ~ /S/ && $7 != ""' titanic.csv | sed 's/female/F/g; s/male/M/g' | awk -F, '{ sum += $7; count++ } END {print sum / count}' 
+awk -F, '$3 == 2 && $NF ~ /S/ && $7 != ""' titanic.csv | sed 's/female/F/g; s/male/M/g' | awk -F, '{print; sum += $7; count++ } END {print "Average age: " sum / count}' 
