10531d25f764e706724bd324c6d948a3b28f3f28
Mia Ameen <59318487+MiaAmeen@users.noreply.github.com>
2024-10-10 16:27:09 -0400

    Update task1.sh

--- task1.sh
+++ task1.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-pid=$(ps -fu $USER | grep "[s]h infinite.sh" | tail -n 1 | awk '{print $2}')
-
-if [ -n "$pid" ]; then
-    # Kill the process
-    kill -9 "$pid"
-    echo "Process killed with PID: $pid"
-else
-    echo "No infinite.sh process found."
-fi
+kill -9 "$(ps -fu $USER | grep "[s]h infinite.sh" | tail -n 1 | awk '{print $2}')"
